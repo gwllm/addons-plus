3b0bfe8845796efcc2ae9d4b51d79525c69dc5a4
DavidGenoy <davidgenoy@gmail.com>
2016-02-19 22:25:35 -0500

    '[ADD]' Ejemplo de vistas tipo formulario, arbol y manejo de nootebook

--- openacademy/openacademy.py
+++ openacademy/openacademy.py
@@ -5,7 +5,9 @@ from openerp import models,fields
 class openacademy_course(models.Model):
     _name = 'openacademy.course'
     _description = 'Esta es la descripcion del curso'
+    
     name= fields.Char('Name')
+    description= fields.Text("Description")
 
 class openacademy_session(models.Model):
     _name = 'openacademy.session'
